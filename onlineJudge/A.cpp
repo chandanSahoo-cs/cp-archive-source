71ab17c180708d33e1b44e12b87beb3840349af6
chandan <chandansahoo02468@gmail.com>
2025-05-14 14:57:42 +0530

    testing

--- onlineJudge/A.cpp
+++ onlineJudge/A.cpp
@@ -1,20 +1,45 @@
 #include "bits/stdc++.h"
 using namespace std;
 
-#define velociraptor ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define velociraptor ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);                                             
 #define all(v) v.begin(),v.end()
 #define lcd(a,b) a*(b/__gcd(a,b))
 #define ll long long
 
-void realmsDomain(){
-    
-}
+const int mod = 1e9+7;
 
+#ifdef chandan  
+#include "starPlatinum.h"
+#define deb(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define deb(x...)
+#endif
+
+void realmsDomain(){
+	ll val; cin>>val;
+    cout<<"Hello"<<"\n";
+}
+g
 int main() {
+    clock_t time_req = clock();
     velociraptor
-    ll tsts = 1 ; cin>>tsts;
+
+
+    #ifdef chandan 
+    freopen("error.txt", "w", stderr); 
+    #endif
+
+    ll tsts = 1 ; 
+
+    cin>>tsts;    
+
     for(ll testcase = 1 ; testcase <=  tsts ; testcase++ ){
         realmsDomain();
     }
+
+    #ifdef chandan
+    cerr << "Time : " << fixed << setprecision(6) << ((double)(clock() - time_req)) / CLOCKS_PER_SEC << endl;
+    #endif
+
     return 0;
 }
